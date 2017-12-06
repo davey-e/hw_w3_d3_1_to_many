a8a6ecde29342118c8bfc4b9604d3be5c7a3ef9d
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-06 16:42:45 +0000

    Added Album class and data to test in console.

--- console.rb
+++ console.rb
@@ -1,9 +1,18 @@
 require('pry-byebug')
 require_relative('./models/artist.rb')
+require_relative('./models/album.rb')
 
 artist1 = Artist.new({'name' => 'Prince'})
 
 artist1.save()
+
+album1 = Album.new({
+  'title' => 'Purple Rain',
+  'genre' => 'Rock',
+  'artist_id' => artist1.id
+  })
+
+  album1.save()
 
 binding.pry
 nil
---
+++ models/album.rb
@@ -0,0 +1,30 @@
+require_relative('../db/sql_runner.rb')
+require_relative('artist.rb')
+
+class Album
+
+attr_reader :id, :artist_id
+attr_accessor :title, :genre
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+  end
+
+def save()
+  sql = "INSERT INTO albums (
+  title,
+  genre,
+  artist_id
+  ) VALUES (
+    $1, $2, $3
+    ) RETURNING *
+    ;"
+    values = [@title, @genre, @artist_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i
+end
+
+end
