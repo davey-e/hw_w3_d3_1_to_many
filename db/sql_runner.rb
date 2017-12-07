1de8c3f72d5379fb333f27fd8f7c508372dbc8b3
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-07 10:17:40 +0000

    Added comments in sql_runner.rb to show how error handling could be added

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -11,3 +11,24 @@ class SqlRunner
   end
 
 end
+
+#SqlRunner with error handling
+# require('pg')
+#
+# class SqlRunner
+#
+#   def self.run(sql, values)
+#     begin
+#       db = PG.connect({dbname: "music_library", host: "localhost"})
+#       db.prepare("query", sql)
+#       result = db.exec_prepared("query", values)
+#     rescue PG::ConnectioBad
+#       p "Bad connection, check connection strings"
+#     ensure
+#       p "Closing database"
+#       db.close() if db != nil
+#     end
+#     return result
+#   end
+#
+# end
